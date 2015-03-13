c4e13ee838964f5b559c9dd234b276d154ec6b2c
MKS-Student <mks.student@hackreactor.com>
2015-03-13 15:38:46 -0700

    Enemies in svg element.

--- watchout.js
+++ watchout.js
@@ -1,2 +1,30 @@
 // start slingin' some d3 here.
 
+var gameOptions = {
+	height: 450,
+	width: 700,
+	nEnemies: 30,
+	padding: 20
+}
+
+var gameBoard = d3.select('body').append('svg:svg')
+						.attr("width", gameOptions.width)
+						.attr("height", gameOptions.height)
+						.style("background-color", "blue");
+
+var enemies = [];
+
+for (var i = 0; i<gameOptions.nEnemies; i++) {
+	var data = {cx: Math.random()*gameOptions.width,
+							cy: Math.random()*gameOptions.height,
+							index:i}
+	enemies.push(data);
+}
+
+gameBoard.selectAll('circle').data(enemies)
+					.enter()
+					.append('circle')
+					.attr("cx", function(d){return d.cx;})
+					.attr("cy", function(d){return d.cy;})
+					.attr("r", 10);
+
