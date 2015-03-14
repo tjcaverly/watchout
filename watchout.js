41ee9f8ca8a8a5fd88af8ffb32b0f3fdf5c5ae8c
MKS-Student <mks.student@hackreactor.com>
2015-03-13 20:12:29 -0700

    Player detects collisions.

--- watchout.js
+++ watchout.js
@@ -3,7 +3,7 @@
 var gameOptions = {
 	height: 450,
 	width: 700,
-	nEnemies: 30,
+	nEnemies: 1,
 	padding: 20
 }
 
@@ -15,15 +15,25 @@ var gameBoard = d3.select('body').append('svg:svg')
 var enemies = [];
 
 for (var i = 0; i<gameOptions.nEnemies; i++) {
-	enemies.push(i);
+	enemies.push({index:i, 
+								x:Math.random()*gameOptions.width,
+							 	y:Math.random()*gameOptions.width,
+							 	r:50
+							 });
 }
 
 var update = function() {
 	gameBoard.selectAll('circle.enemy')
 	         .transition()
-	         .duration(1000)
-					 .attr("cx", function(d){return Math.random()*gameOptions.width;})
-					 .attr("cy", function(d){return Math.random()*gameOptions.height;});
+	         .duration(5000)
+					 .attr("cx", function(d){
+					 	var x = Math.random()*gameOptions.width;
+					 	d.x = x;
+					 	return x;})
+					 .attr("cy", function(d){
+					 	var y = Math.random()*gameOptions.height;
+					 	d.y = y;
+					 	return y;});
 
 }
 var drag = d3.behavior.drag()
@@ -41,11 +51,11 @@ gameBoard.selectAll('circle.enemy').data(enemies)
 					.enter()
 					.append('circle')
 					.attr('class', 'enemy')
-					.attr("cx", function(d){return Math.random()*gameOptions.width;})
-					.attr("cy", function(d){return Math.random()*gameOptions.height;})
-					.attr("r", 10);
+					.attr("cx", function(d){return d.x;})
+					.attr("cy", function(d){return d.y;})
+					.attr("r", 50);
 
-gameBoard.selectAll('circle.player').data([{x:100, y:100, r:10}])
+gameBoard.selectAll('circle.player').data([{x:100, y:100, r:50}])
 						.enter()
 						.append('circle')
 						.attr('class', 'player')
@@ -55,6 +65,31 @@ gameBoard.selectAll('circle.player').data([{x:100, y:100, r:10}])
 						.attr("fill", "red")
 						.call(drag);
 
+d3.timer(function(){
+	//console.log('hi');
+	var player = gameBoard.select('circle.player').data()[0];
+	var enemies = gameBoard.selectAll("circle.enemy")
+	enemies.each(function(){
+		var enemy = d3.select(this);
+		var enemyCircle = {
+			x:parseFloat(enemy.attr('cx')),
+			y:parseFloat(enemy.attr('cy')),
+			r: 50
+		}
+		collision(player, enemyCircle)
+	});
 
-setInterval(update, 1000);
+
+});
+
+
+
+var collision = function(c1, c2) {
+
+	var dist = Math.sqrt( Math.pow( (c1.x - c2.x), 2) + Math.pow( (c1.y - c2.y), 2) );
+	return dist < c1.r + c2.r;
+}
+
+
+setInterval(update, 5000);
 
